64f389bb310e479d06c3c35f72847200d3a72077
rekha528 <78066522+rekha528@users.noreply.github.com>
2021-02-01 15:59:38 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch("../data/data.json").then(response=>{
+fetch("https://rekha528.github.io/ecommerce/data/data.json").then(response=>{
 	return response.json()
 }).then(data=>{
 	displayData(data)
